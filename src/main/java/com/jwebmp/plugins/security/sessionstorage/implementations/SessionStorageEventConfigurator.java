e7988b89310bcc13bde720215793f344f335d6a5
GedMarc <ged_marc@hotmail.com>
2020-09-05 13:23:35 +0200

    Split ZSTD-JNI out, and add in the kafka client module

--- src/main/java/com/jwebmp/plugins/security/sessionstorage/implementations/SessionStorageEventConfigurator.java
+++ src/main/java/com/jwebmp/plugins/security/sessionstorage/implementations/SessionStorageEventConfigurator.java
@@ -2,16 +2,17 @@ package com.jwebmp.plugins.security.sessionstorage.implementations;
 
 import com.jwebmp.core.Event;
 import com.jwebmp.core.events.IEventConfigurator;
-import com.jwebmp.core.utilities.StaticStrings;
 
 import javax.validation.constraints.NotNull;
+
+import static com.jwebmp.core.utilities.StaticStrings.*;
 
 public class SessionStorageEventConfigurator implements IEventConfigurator<SessionStorageEventConfigurator>
 {
 	@Override
 	public @NotNull Event<?, ?> configureEvent(Event event)
 	{
-		event.returnVariable(StaticStrings.SESSION_STORAGE_PARAMETER_KEY);
+		event.returnVariable(SESSION_STORAGE_VARIABLE_KEY);
 		return event;
 	}
 }
