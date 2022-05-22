1b4106d2646cc4d8bd0cf90303905ea7c0185b9e
GedMarc <marc.magon@gmail.com>
2022-05-22 18:11:23 +0200

    Updates to counter up, simple bar, waypoints, local storage sessions storage, and waves for TS

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,23 +3,14 @@ import com.jwebmp.plugins.security.sessionstorage.SessionStoragePageConfigurator
 import com.jwebmp.plugins.security.sessionstorage.implementations.*;
 
 module com.jwebmp.plugins.security.sessionstorage {
-
-	requires com.jwebmp.core;
-	requires jakarta.validation;
-	requires com.google.guice;
-	requires com.fasterxml.jackson.databind;
+	
+	requires transitive com.jwebmp.core.angular;
 	requires com.google.guice.extensions.servlet;
-	requires com.guicedee.guicedinjection;
 	requires com.guicedee.guicedservlets.websockets;
 	requires jakarta.websocket.api;
 
 	provides com.jwebmp.core.services.IPageConfigurator with SessionStoragePageConfigurator;
-	provides com.jwebmp.core.events.IEventConfigurator with SessionStorageEventConfigurator;
-	provides com.jwebmp.interception.services.AjaxCallIntercepter with SessionStorageIntercepter;
-//	provides com.jwebmp.interception.services.DataCallIntercepter with SessionStorageIntercepter;
-	provides com.guicedee.guicedservlets.websockets.services.IWebSocketAuthDataProvider with SessionStorageKeyWSAuth;
 	provides com.guicedee.guicedservlets.websockets.services.IWebSocketMessageReceiver with SessionStorageWSMessageReceiver;
-//	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleInclusions with SessionStorageInclusionModule;
 	provides IGuiceModule with SessionStorageSecurityBinder;
 	
 	exports com.jwebmp.plugins.security.sessionstorage.implementations;
--- src/main/java/com/jwebmp/plugins/security/sessionstorage/SessionStoragePageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/security/sessionstorage/SessionStoragePageConfigurator.java
@@ -2,6 +2,7 @@ package com.jwebmp.plugins.security.sessionstorage;
 
 import com.google.inject.Singleton;
 import com.jwebmp.core.Page;
+import com.jwebmp.core.base.angular.services.annotations.structures.*;
 import com.jwebmp.core.plugins.PluginInformation;
 import com.jwebmp.core.plugins.PluginStatus;
 import com.jwebmp.core.services.IPageConfigurator;
@@ -10,25 +11,27 @@ import jakarta.validation.constraints.NotNull;
 
 @Singleton
 @PluginInformation(pluginName = "Session Storage ",
-		pluginUniqueName = "session-storage",
-		pluginDescription = "Provides access to read and write into the session (per-tab) storage of a browser.",
-		pluginVersion = "1.0",
-		pluginDependancyUniqueIDs = "jquery",
-		pluginCategories = "storage,cookies,local-storage,session-storage",
-		pluginSubtitle = "Session Storage Service ",
-		pluginGitUrl = "https://github.com/GedMarc/JWebMP-SessionStorageSecurity",
-		pluginSourceUrl = "https://github.com/GedMarc/JWebMP-SessionStorageSecurity",
-		pluginWikiUrl = "https://github.com/GedMarc/JWebMP-SessionStorageSecurity/wiki",
-		pluginOriginalHomepage = "https://github.com/GedMarc/JWebMP-SessionStorageSecurity",
-		pluginDownloadUrl = "https://mvnrepository.com/artifact/com.jwebmp.plugins.security/jwebmp-plugins-session-storage-security",
-		pluginIconUrl = "",
-		pluginIconImageUrl = "",
-		pluginLastUpdatedDate = "2022/03/07",
-		pluginGroupId = "com.jwebmp.plugins.security",
-		pluginArtifactId = "jwebmp-plugins-session-storage-security",
-		pluginModuleName = "com.jwebmp.plugins.security.sessionstorage",
-		pluginStatus = PluginStatus.Released
+                   pluginUniqueName = "session-storage",
+                   pluginDescription = "Provides access to read and write into the session (per-tab) storage of a browser.",
+                   pluginVersion = "1.0",
+                   pluginDependancyUniqueIDs = "jquery",
+                   pluginCategories = "storage,cookies,local-storage,session-storage",
+                   pluginSubtitle = "Session Storage Service ",
+                   pluginGitUrl = "https://github.com/GedMarc/JWebMP-SessionStorageSecurity",
+                   pluginSourceUrl = "https://github.com/GedMarc/JWebMP-SessionStorageSecurity",
+                   pluginWikiUrl = "https://github.com/GedMarc/JWebMP-SessionStorageSecurity/wiki",
+                   pluginOriginalHomepage = "https://github.com/GedMarc/JWebMP-SessionStorageSecurity",
+                   pluginDownloadUrl = "https://mvnrepository.com/artifact/com.jwebmp.plugins.security/jwebmp-plugins-session-storage-security",
+                   pluginIconUrl = "",
+                   pluginIconImageUrl = "",
+                   pluginLastUpdatedDate = "2022/03/07",
+                   pluginGroupId = "com.jwebmp.plugins.security",
+                   pluginArtifactId = "jwebmp-plugins-session-storage-security",
+                   pluginModuleName = "com.jwebmp.plugins.security.sessionstorage",
+                   pluginStatus = PluginStatus.Released
 )
+@NgBootConstructorParameter("private socketClientService : SocketClientService")
+@NgBootConstructorBody(value = "this.socketClientService.send('SessionStorage',{},'sessionStorage');", sortOrder = 105)
 public class SessionStoragePageConfigurator
 		implements IPageConfigurator<SessionStoragePageConfigurator>
 {
@@ -36,20 +39,20 @@ public class SessionStoragePageConfigurator
 	 * The session storage
 	 */
 	private boolean sessionStorage = true;
-
+	
 	@Override
 	public @NotNull Page<?> configure(Page<?> page)
 	{
 		return page;
 	}
-
+	
 	@Override
 	public boolean enabled()
 	{
 		return sessionStorage;
 	}
-
-
+	
+	
 	/**
 	 * If this page should be rendered with dynamic local storage support
 	 *
@@ -59,7 +62,7 @@ public class SessionStoragePageConfigurator
 	{
 		return sessionStorage;
 	}
-
+	
 	/**
 	 * If the page should be rendered with dynamic local storage support. This renders a default page that is then fetched from the server to support cordova applications
 	 *
