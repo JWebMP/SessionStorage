50725b88d20834ce1232432fa83d6d42e15958c0
GedMarc <marc.magon@gmail.com>
2024-03-02 20:36:41 +0200

    Add Browser Storage

--- src/main/java/com/jwebmp/plugins/security/sessionstorage/implementations/SessionStorageStringProvider.java
+++ src/main/java/com/jwebmp/plugins/security/sessionstorage/implementations/SessionStorageStringProvider.java
@@ -1,7 +1,7 @@
 package com.jwebmp.plugins.security.sessionstorage.implementations;
 
 import com.google.inject.*;
-import com.guicedee.guicedinjection.*;
+import com.guicedee.client.IGuiceContext;
 import com.jwebmp.core.base.ajax.*;
 
 import java.util.*;
@@ -10,32 +10,32 @@ import static com.jwebmp.core.utilities.StaticStrings.*;
 
 public class SessionStorageStringProvider implements Provider<String>
 {
-	@Override
-	public String get()
-	{
-		AjaxCall<?> call = GuiceContext.get(AjaxCall.class);
-		com.jwebmp.core.base.ajax.AjaxResponse<?> response = GuiceContext.get(com.jwebmp.core.base.ajax.AjaxResponse.class);
-		UUID uuid = null;
-		if (call.getSessionStorage()
-		        .isEmpty())
-		{
-			return null;
-		}
-		
-		if (call.getSessionStorage()
-		        .containsKey(SESSION_STORAGE_PARAMETER_KEY))
-		{
-			return call.getSessionStorage()
-			           .get(SESSION_STORAGE_PARAMETER_KEY);
-		}
-		
-		uuid = UUID.randomUUID();
-		call.getSessionStorage()
-		    .put(SESSION_STORAGE_PARAMETER_KEY, uuid.toString());
-		response.getSessionStorage()
-		        .put(SESSION_STORAGE_PARAMETER_KEY, uuid.toString());
-		
-		return uuid.toString();
-	}
-	
+    @Override
+    public String get()
+    {
+        AjaxCall<?> call = IGuiceContext.get(AjaxCall.class);
+        com.jwebmp.core.base.ajax.AjaxResponse<?> response = IGuiceContext.get(com.jwebmp.core.base.ajax.AjaxResponse.class);
+        UUID uuid = null;
+        if (call.getSessionStorage()
+                .isEmpty())
+        {
+            return null;
+        }
+
+        if (call.getSessionStorage()
+                .containsKey(SESSION_STORAGE_PARAMETER_KEY))
+        {
+            return call.getSessionStorage()
+                       .get(SESSION_STORAGE_PARAMETER_KEY);
+        }
+
+        uuid = UUID.randomUUID();
+        call.getSessionStorage()
+            .put(SESSION_STORAGE_PARAMETER_KEY, uuid.toString());
+        response.getSessionStorage()
+                .put(SESSION_STORAGE_PARAMETER_KEY, uuid.toString());
+
+        return uuid.toString();
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,20 +4,23 @@ import com.jwebmp.plugins.security.sessionstorage.implementations.SessionStorage
 import com.jwebmp.plugins.security.sessionstorage.implementations.SessionStorageWSMessageReceiver;
 
 module com.jwebmp.plugins.security.sessionstorage {
-	
-	requires transitive com.jwebmp.core.angular;
-	requires com.google.guice.extensions.servlet;
-	requires com.guicedee.guicedservlets.websockets;
-	requires jakarta.websocket;
-	requires jakarta.websocket.client;
-	requires static lombok;
 
-	provides com.jwebmp.core.services.IPageConfigurator with SessionStoragePageConfigurator;
-	provides com.guicedee.guicedservlets.websockets.services.IWebSocketMessageReceiver with SessionStorageWSMessageReceiver;
-	provides IGuiceModule with SessionStorageSecurityBinder;
-	
-	exports com.jwebmp.plugins.security.sessionstorage.implementations;
-	opens com.jwebmp.plugins.security.sessionstorage.implementations to com.google.guice,com.fasterxml.jackson.databind;
-	exports com.jwebmp.plugins.security.sessionstorage;
-	opens com.jwebmp.plugins.security.sessionstorage to com.google.guice,com.fasterxml.jackson.databind;
+    requires transitive com.jwebmp.core.base.angular.client;
+    requires com.jwebmp.client;
+    requires com.jwebmp.core;
+
+    requires com.google.guice.extensions.servlet;
+    requires com.guicedee.guicedservlets.websockets;
+    requires jakarta.websocket;
+    requires jakarta.websocket.client;
+    requires static lombok;
+
+    provides com.jwebmp.core.services.IPageConfigurator with SessionStoragePageConfigurator;
+    provides com.guicedee.guicedservlets.websockets.services.IWebSocketMessageReceiver with SessionStorageWSMessageReceiver;
+    provides IGuiceModule with SessionStorageSecurityBinder;
+
+    exports com.jwebmp.plugins.security.sessionstorage.implementations;
+    opens com.jwebmp.plugins.security.sessionstorage.implementations to com.google.guice, com.fasterxml.jackson.databind;
+    exports com.jwebmp.plugins.security.sessionstorage;
+    opens com.jwebmp.plugins.security.sessionstorage to com.google.guice, com.fasterxml.jackson.databind;
 }
